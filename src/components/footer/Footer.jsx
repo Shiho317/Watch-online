297b5dbd711d876639c494535f5d0ce6bc25a100
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-05-07 11:49:12 -0700

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { RiFacebookBoxLine, RiTwitterLine, RiInstagramLine, RiLinkedinBoxLine } from 'react-icons/ri'
+import "./Footer.style.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer-section">
+        <div className="footer-list">
+          <ul>
+            <li>
+              <Link to="/cart">Your Cart</Link>
+            </li>
+            <li>
+              <Link to="/">Your Orders</Link>
+            </li>
+            <li>
+              <Link to="/service">Returns</Link>
+            </li>
+            <li>
+              <Link to="/service">Customer Service</Link>
+            </li>
+            <li>
+              <Link to="/service">Contacts</Link>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-icon">
+          <div class="icon">
+            <RiTwitterLine />
+          </div>
+          <div class="icon">
+            <RiFacebookBoxLine />
+          </div>
+          <div class="icon">
+            <RiLinkedinBoxLine />
+          </div>
+          <div class="icon">
+            <RiInstagramLine />
+          </div>
+        </div>
+      </div>
+      <div class="copyright">
+        <h6>Copyright ©2022 ShihoN All Rights Reserved.</h6>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
